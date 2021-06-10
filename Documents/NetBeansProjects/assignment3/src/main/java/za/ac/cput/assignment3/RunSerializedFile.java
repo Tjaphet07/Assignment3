7f1967f513aa51609af6c0a1ee36e43d16ee6f54
Ted Japhet NZAMBA MABIKA <nztedjaphet@gmail.com>
2021-06-10 11:35:14 +0200

    first commit

---
+++ Documents/NetBeansProjects/assignment3/src/main/java/za/ac/cput/assignment3/RunSerializedFile.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.assignment3;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
+
+/**
+ *
+ * @author 219197695 Ted Japhet NZAMBA MABIKA
+ */
+public class RunSerializedFile {
+    
+    public static void main(String[] args) {
+        
+        ArrayList<Customer> cust=new ArrayList<Customer>();
+        ArrayList<Supplier> sup=new ArrayList<Supplier>(); 
+
+    }
+}
